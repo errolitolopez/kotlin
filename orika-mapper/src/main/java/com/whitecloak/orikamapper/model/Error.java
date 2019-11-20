f14d86bc879c053c9f364e1fc24f5ea47750d98e
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-20 14:01:16 +0800

    Add Error model

---
+++ orika-mapper/src/main/java/com/whitecloak/orikamapper/model/Error.java
@@ -0,0 +1,16 @@
+package com.whitecloak.orikamapper.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Error {
+
+    private int code;
+    private String message;
+}
