78c7578f76f83abcdcf81f87449b9e636a9661c8
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-20 14:01:01 +0800

    Add SourceForm

---
+++ orika-mapper/src/main/java/com/whitecloak/orikamapper/request/SourceForm.java
@@ -0,0 +1,17 @@
+package com.whitecloak.orikamapper.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonDeserialize
+public class SourceForm {
+
+    private int age;
+}
