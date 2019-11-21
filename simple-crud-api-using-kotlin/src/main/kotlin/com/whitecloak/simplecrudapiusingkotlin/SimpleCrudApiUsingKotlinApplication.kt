f7761844514b8d07eddddc005f6d5e027b9875f3
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-21 18:44:11 +0800

    - Add LoggerFactory - log "Hello Kotlin!"

--- simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplication.kt
+++ simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplication.kt
@@ -1,11 +1,16 @@
 package com.whitecloak.simplecrudapiusingkotlin
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SimpleCrudApiUsingKotlinApplication
 
+private val log = LoggerFactory.getLogger(SimpleCrudApiUsingKotlinApplication::class.java)
+
 fun main(args: Array<String>) {
-	runApplication<SimpleCrudApiUsingKotlinApplication>(*args)
+    runApplication<SimpleCrudApiUsingKotlinApplication>(*args)
+    log.info("Hello Kotlin!")
+
 }
