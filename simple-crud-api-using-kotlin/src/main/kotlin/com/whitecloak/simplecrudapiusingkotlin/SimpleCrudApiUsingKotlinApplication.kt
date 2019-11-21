f085cf12ce5b619fb872b607de6de28e40c4e0f8
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-21 16:42:37 +0800

    Initial commit

---
+++ simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.whitecloak.simplecrudapiusingkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleCrudApiUsingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleCrudApiUsingKotlinApplication>(*args)
+}
---
+++ simple-crud-api-using-kotlin/src/test/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whitecloak.simplecrudapiusingkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleCrudApiUsingKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
