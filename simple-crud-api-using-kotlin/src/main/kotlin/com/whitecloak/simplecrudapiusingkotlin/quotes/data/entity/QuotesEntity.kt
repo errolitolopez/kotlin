f5bcd9115d3f7de13951453228c3a056f2e7dfef
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-22 16:14:32 +0800

    Change id type to UUID

--- simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/quotes/data/entity/QuotesEntity.kt
+++ simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/quotes/data/entity/QuotesEntity.kt
@@ -2,11 +2,12 @@ package com.whitecloak.simplecrudapiusingkotlin.quotes.data.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.UUID
 
 @Document("Quotes")
 data class QuotesEntity(
         @Id
-        var _id: String? = null,
+        var _id: UUID? = null,
         var body: String? = null,
         var author: String? = null
 )
