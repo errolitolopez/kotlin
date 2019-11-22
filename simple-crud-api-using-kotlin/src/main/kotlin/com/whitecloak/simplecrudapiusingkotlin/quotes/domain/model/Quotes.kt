57f8517ccfcb90d70c6e8a9adce5b95b07c86371
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-22 10:45:23 +0800

    Add domain model Quotes

---
+++ simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/quotes/domain/model/Quotes.kt
@@ -0,0 +1,7 @@
+package com.whitecloak.simplecrudapiusingkotlin.quotes.domain.model
+
+data class Quotes (
+        var _id: String? = null,
+        var body: String? = null,
+        var author: String? = null
+)
