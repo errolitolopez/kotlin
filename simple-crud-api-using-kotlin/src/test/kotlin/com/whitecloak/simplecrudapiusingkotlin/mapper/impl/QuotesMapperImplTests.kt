f2e29387526df9c95b91de8661e2f4bdf6d873c1
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-11-22 16:12:35 +0800

    - Refactor function name - Add 2 new test mapFormToModel & mapModelToResource

--- simple-crud-api-using-kotlin/src/test/kotlin/com/whitecloak/simplecrudapiusingkotlin/mapper/impl/QuotesMapperImplTests.kt
+++ simple-crud-api-using-kotlin/src/test/kotlin/com/whitecloak/simplecrudapiusingkotlin/mapper/impl/QuotesMapperImplTests.kt
@@ -1,5 +1,7 @@
 package com.whitecloak.simplecrudapiusingkotlin.mapper.impl
 
+import com.whitecloak.simplecrudapiusingkotlin.quotes.api.request.QuotesForm
+import com.whitecloak.simplecrudapiusingkotlin.quotes.api.response.QuotesResource
 import com.whitecloak.simplecrudapiusingkotlin.quotes.data.entity.QuotesEntity
 import com.whitecloak.simplecrudapiusingkotlin.quotes.data.mapper.impl.QuotesMapperImpl
 import com.whitecloak.simplecrudapiusingkotlin.quotes.domain.model.Quotes
@@ -8,6 +10,8 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
 import org.mockito.MockitoAnnotations
+import org.slf4j.LoggerFactory
+import java.util.UUID
 
 class QuotesMapperImplTests {
 
@@ -16,36 +20,67 @@ class QuotesMapperImplTests {
         MockitoAnnotations.initMocks(this)
     }
 
-//    val log = LoggerFactory.getLogger(SimpleCrudApiUsingKotlinApplication::class.java)
+    private val log = LoggerFactory.getLogger(QuotesMapperImplTests::class.java)
 
     @InjectMocks
     val quotesMapperImpl = QuotesMapperImpl()
 
     @Test
-    fun map_whenQuotesIsGiven_thenCorrect() {
+    fun mapModelToEntity_whenQuotesIsGiven_thenCorrect() {
+        val quotesId = UUID.randomUUID()
+
         val actual = Quotes()
-        actual._id = "123"
+        actual._id = quotesId
         actual.author = "Errolito Lopez"
         actual.body = "It is a fantastic energy, sir."
 
         val expected = QuotesEntity()
-        expected._id = "123"
+        expected._id = quotesId
         expected.author = "Errolito Lopez"
         expected.body = "It is a fantastic energy, sir."
-        assertEquals(expected, quotesMapperImpl.map(actual))
+        assertEquals(expected, quotesMapperImpl.mapModelToEntity(actual))
     }
 
     @Test
-    fun map_whenQuotesEntityIsGiven_thenCorrect() {
+    fun mapEntityToModel_whenQuotesEntityIsGiven_thenCorrect() {
+        val quotesId = UUID.randomUUID()
+
         val actual = QuotesEntity()
-        actual._id = "123"
+        actual._id = quotesId
         actual.author = "Errolito Lopez"
         actual.body = "It is a fantastic energy, sir."
 
         val expected = Quotes()
-        expected._id = "123"
+        expected._id = quotesId
         expected.author = "Errolito Lopez"
         expected.body = "It is a fantastic energy, sir."
-        assertEquals(expected, quotesMapperImpl.map(actual))
+        assertEquals(expected, quotesMapperImpl.mapEntityToModel(actual))
+    }
+
+    @Test
+    fun mapFormToModel_whenQuotesFormIsGiven_thenCorrect() {
+        val actual = QuotesForm()
+        actual.author = "Errolito Lopez"
+        actual.body = "It is a fantastic energy, sir."
+
+        val expected = Quotes()
+        expected.author = "Errolito Lopez"
+        expected.body = "It is a fantastic energy, sir."
+        assertEquals(expected, quotesMapperImpl.mapFormToModel(actual))
+    }
+
+    @Test
+    fun mapModelToResource_whenQuotesIsGiven_thenCorrect() {
+        val quotesId = UUID.randomUUID()
+
+        val actual = Quotes()
+        actual._id = quotesId
+        actual.author = "Errolito Lopez"
+        actual.body = "String"
+
+        val expected = QuotesResource()
+        expected.author = "Errolito Lopez"
+        expected.body = "String"
+        assertEquals(expected, quotesMapperImpl.mapModelToResource(actual))
     }
 }
