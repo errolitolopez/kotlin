39f752ecaa699bbf2e1eb6f7e7dad4f3ca22724d
Errolito Lopez <errolito.lopez@whitecloak.com>
2019-12-05 11:17:25 +0800

    Add CommandLineRunner

--- simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplication.kt
+++ simple-crud-api-using-kotlin/src/main/kotlin/com/whitecloak/simplecrudapiusingkotlin/SimpleCrudApiUsingKotlinApplication.kt
@@ -1,10 +1,19 @@
 package com.whitecloak.simplecrudapiusingkotlin
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SimpleCrudApiUsingKotlinApplication
+class SimpleCrudApiUsingKotlinApplication {
+
+    @Bean
+    fun init(): CommandLineRunner = CommandLineRunner {
+
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SimpleCrudApiUsingKotlinApplication>(*args)
